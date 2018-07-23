133d1f90eeff12025079dc9a1908066acd24823b
Geirr Winnem <gwinnem@gmail.com>
2018-07-23 08:23:15 +0200

    Initial documentation files

---
+++ documentation/js/app/abTempSvc.js
@@ -0,0 +1,19 @@
+﻿/**
+ * 
+ * @author Geirr Winnem
+ * @version 1.0.0
+ * @link http://www.abadmin.com
+ * @license MIT
+ * @summary 
+ */
+
+(function () {
+    "use strict";
+    angular.module("abTemModule", [[]])
+        .factory("tempSvc", ["abDataSvc", "config", function (abDataSvc, config) {
+
+            return {
+
+            };
+        }]);
+})();

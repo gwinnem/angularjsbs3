0717119d084e0a42e70f69fbe1b71931d8b1a44c
Geirr Winnem <gwinnem@gmail.com>
2018-07-23 09:11:59 +0200

    Open close treeview fix

--- js/app/abTree.js
+++ js/app/abTree.js
@@ -14,6 +14,8 @@
 
     var Default = {
         animationSpeed: 500,
+        accordion: true,
+        followLink: false,
         trigger: '.treeview a',
         triggertoplevel: '.tree-menu a'
     };
@@ -86,14 +88,15 @@
 
         if (!parentLi.is(Selector.treeview)) {
             // Removing all active classes before adding it to the element clicked
-            $(Selector.sidebarMenu).find(Selector.active).each(function () {
+            $(Selector.sidebarMenu).find(Selector.active)
+                .each(function () {
                     $(this).removeClass(ClassName.active);
                 });
             parentLi.addClass(ClassName.active);
             return;
         }
 
-        if (link.attr('href') === '#') {
+        if (!this.options.followLink || link.attr('href') === '#') {
             event.preventDefault();
         }
 
@@ -121,6 +124,7 @@
 
     Tree.prototype.collapse = function (tree, parentLi) {
         var collapsedEvent = $.Event(Event.collapsed);
+
         tree.find(Selector.open).removeClass(ClassName.open);
         parentLi.removeClass(ClassName.open);
         tree.slideUp(this.options.animationSpeed, function () {
@@ -178,3 +182,78 @@
     });
 
 }(jQuery);
+
+
+//     Tree.prototype.toggletoplevel = function (element, event) {
+//         // Do nothing if the active menu item is clicked
+//         if ($(event.currentTarget.parentElement).hasClass(ClassName.active)) {
+//             return;
+//         };
+
+//         // Removing all open classes from treeview menu
+//         $(Selector.treeviewMenu + Selector.open).each(function () {
+//             $(this).removeClass(ClassName.active);
+//         });
+
+//         // adding active class to the selected top level menu item
+//         $(element.context.parentElement).addClass(ClassName.active);
+
+//         //$(Selector.treeMenu + " > " + Selector.open).find(Selector.open + ' > ' + Selector.treeview).slideUp();
+//         //$(Selector.treeviewMenu + Selector.open).slideUp();
+//         // var that = $(Selector.open + ' > ' + Selector.treeviewMenu);
+//         // $(Selector.open + ' > ' + Selector.treeviewMenu).each(function () {
+//         //     $(this).removeClass(ClassName.open);
+//         //     //$(selector).slideUp(speed,easing,callback)
+//         // });
+//         return;
+//     };
+
+//     Tree.prototype.toggle = function (link, event) {
+//         var treeviewMenu = link.next(Selector.treeviewMenu);
+//         var parentLi = link.parent();
+//         var isOpen = parentLi.hasClass(ClassName.open);
+
+//         if (!parentLi.is(Selector.treeview)) {
+//             // Removing all active classes before adding it to the element clicked
+//             $(Selector.sidebarMenu).find(Selector.active).each(function () {
+//                     $(this).removeClass(ClassName.active);
+//                 });
+//             parentLi.addClass(ClassName.active);
+//             return;
+//         }
+
+//         if (link.attr('href') === '#') {
+//             event.preventDefault();
+//         }
+
+//         if (isOpen) {
+//             this.collapse(treeviewMenu, parentLi);
+//         } else {
+//             this.expand(treeviewMenu, parentLi);
+//         }
+//     };
+
+//     Tree.prototype.expand = function (tree, parent) {
+//         var expandedEvent = $.Event(Event.expanded);
+
+//         if (this.options.accordion) {
+//             var openMenuLi = parent.siblings(Selector.open);
+//             var openTree = openMenuLi.children(Selector.treeviewMenu);
+//             this.collapse(openTree, openMenuLi);
+//         }
+
+//         parent.addClass(ClassName.open);
+//         tree.slideDown(this.options.animationSpeed, function () {
+//             $(this.element).trigger(expandedEvent);
+//         }.bind(this));
+//     };
+
+//     Tree.prototype.collapse = function (tree, parentLi) {
+//         var collapsedEvent = $.Event(Event.collapsed);
+//         tree.find(Selector.open).removeClass(ClassName.open);
+//         parentLi.removeClass(ClassName.open);
+//         tree.slideUp(this.options.animationSpeed, function () {
+//             tree.find(Selector.open + ' > ' + Selector.treeview).slideUp();
+//             $(this.element).trigger(collapsedEvent);
+//         }.bind(this));
+//     };
